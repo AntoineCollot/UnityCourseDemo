b2a738165e3332d55a5249bdecc6e6c59d8fa004
AntoineCollot <antoine.collot@live.com>
2019-02-03 02:41:40 +0100

    Added particles explosion

---
+++ Assets/Turret/EmitParticlesOnCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitParticlesOnCollision : MonoBehaviour {
+
+    public GameObject particlesPrefab;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Spawn a particle effect at its position
+        GameObject newParticles = Instantiate(particlesPrefab, transform.position, Quaternion.identity, null);
+        //Destroy the particles in 1 sec
+        Destroy(newParticles, 1);
+
+        //Destroy itself
+        Destroy(gameObject);
+
+    }
+}
